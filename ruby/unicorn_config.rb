4be7938d944ecc7cc225fe3d956814e8e3da5130
Nobuhiro Nikushi <deneb.ge@gmail.com>
2013-11-04 15:57:22 +0900

    Try GC, but comment out

--- ruby/unicorn_config.rb
+++ ruby/unicorn_config.rb
@@ -1,2 +1,8 @@
-worker_processes 10
+worker_processes 8
 preload_app true
+listen "/tmp/unicorn.sock"
+
+#GC.respond_to?(:copy_on_write_friendly=) and GC.copy_on_write_friendly = true
+#after_fork do |server, worker|
+#  GC.disable
+#end
